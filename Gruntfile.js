4ae3fe6bc267ca2f891100f30aa0231ba3cf57d6
Sean Duncan <seaneduncan@gmail.com>
2013-12-27 07:13:10 -0700

    added much more grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,91 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'css/build/styles.css': 'css/styles.scss'
+        }
+      }
+    },
+
+    concat: {
+      dist: {
+        src: [
+          'js/libs/*.js',
+          'js/global.js'
+        ],
+        dest: 'js/build/production.js'
+      }
+    },
+
+    uglify: {
+      build: {
+        src: 'js/build/production.js',
+        dest: 'js/build/production.min.js'
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'images/'
+        }]
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true,
+      },
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['concat', 'uglify'],
+        options: {
+          spawn: false,
+        }
+      },
+      css: {
+        files: ['css/*.scss'],
+        tasks: ['sass'],
+        options: {
+          spawn: false,
+        }
+      },
+      images: {
+        files: ['images/**/*.{png,jpg,gif}', 'images/*.{png,jpg,gif}'],
+        tasks: ['imagemin'],
+        options: {
+          spawn: false,
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: './'
+        }
+      }
+    },
+
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+
+  grunt.registerTask('default', ['concat', 'uglify', 'sass', 'imagemin']);
+
+  grunt.registerTask('server', ['connect', 'watch']);
+
+};
