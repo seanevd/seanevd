4426a2d897d7da0c6e784378df20ab9c3bfa6f4e
Sean Duncan <seaneduncan@gmail.com>
2014-02-03 15:51:21 -0700

    Added better mobile views and a favicon

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           cwd: 'img/',
-          src: ['**/*.{png,jpg,gif}'],
+          src: ['**/*.{png,jpg,gif,ico}'],
           dest: 'images/'
         }]
       }
